ac94e48ced4bddbe09154d6baca303a20c1aa632
Ankit-191 <godaraankit003@gmail.com>
2023-08-23 10:59:59 +0530

    header start

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import './ankit.css'
-import './sumit.css'
-import "./johny.css"
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import "./ankit.css";
+import "./sumit.css";
+import "./johny.css";
+import AboutUs from "./components/AboutUs";
 const App = () => {
   return (
-    <>
-    </>
-  )
-}
+    <div>
+      <AboutUs />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <>
+        <section>
+            <Container>
+              <div className=''>
+
+              </div>
+            </Container>
+        </section>
+    </>
+  )
+}
+
+export default AboutUs
