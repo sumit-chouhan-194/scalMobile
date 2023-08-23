49d1d9e29de55ba06dae757d3c257e208c83cd87
Your Name <sumitkhan.dhana@gmail.com>
2023-08-23 10:21:34 +0530

    add css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import './ankit.css'
+import './sumit.css'
+import "./johny.css"
 const App = () => {
   return (
     <>
